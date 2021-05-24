7fba853c7180057dc389479484facdd1484674f0
Ramiro Torrejón <ramijtc@gmail.com>
2021-05-23 23:52:16 -0500

    refactor: Used kotlin function extension to verify an empty list

--- app/src/main/java/com/udacity/project4/locationreminders/reminderslist/RemindersListViewModel.kt
+++ app/src/main/java/com/udacity/project4/locationreminders/reminderslist/RemindersListViewModel.kt
@@ -70,6 +70,6 @@ class RemindersListViewModel(
      * Inform the user that there's not any data if the remindersList is empty
      */
     private fun invalidateShowNoData() {
-        showNoData.value = remindersList.value == null || remindersList.value!!.isEmpty()
+        showNoData.value = remindersList.value.isNullOrEmpty()
     }
 }
